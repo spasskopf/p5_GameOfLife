cd8d714aa0af736dcc5420264fb4a0523b4e9498
spasskopf <78727783+spasskopf@users.noreply.github.com>
2021-09-23 18:34:23 +0200

    Added Image loading (works best with black/white images)

--- src/IO.js
+++ src/IO.js
@@ -11,11 +11,10 @@ document.getElementById("save").addEventListener("click", () => {
     saveData();
 });
 
-function read(e) {
-    const file = e.target.files[0];
-    if (!file) {
-        return;
-    }
+/**Loads cell-states from a JSON File
+ * @param {Blob} file
+ */
+function loadFromJSON(file) {
     const reader = new FileReader();
     reader.onload = function (e) {
         const data = e.target.result;
@@ -32,6 +31,73 @@ function read(e) {
         console.log(`Changed state of ${cells.length} cells.`);
     };
     reader.readAsText(file);
+}
+
+/**Loads cell-states from a JSON File
+ * @param {File} file
+ */
+function loadFromImage(file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const binaryData = e.target.result;
+        if (typeof binaryData !== "string") {
+            console.error("Invalid Data!");
+            return;
+        }
+        const base64 = btoa(binaryData);
+        const path = `data:image/png;base64,${base64}`;
+        loadImage(path, (image) => {
+                //Convert to black and white picture
+                image.filter(THRESHOLD);
+                image.loadPixels();
+                const pixels = image.pixels;
+                for (let y = 0; y < image.height; y++) {
+                    for (let x = 0; x < image.width; x++) {
+                        //Because images are RGBA and every channel is stored in a separate value
+                        const startLoc = 4 * (y * image.width + x);
+                        const r = pixels[startLoc],
+                            g = pixels[startLoc + 1],
+                            b = pixels[startLoc + 2];/*,
+                            a = pixels[startLoc + 3];*/
+                        const state = r === 0 && g === 0 && b === 0;
+                        const fieldCell = getCellFromIndex(y, x);
+                        if (!fieldCell) {
+                            break;
+                        }
+                        fieldCell.alive = state;
+                        fieldCell.updateColor(state);
+                    }
+                }
+            }, err => {
+                error("Could not load image!", err);
+            }
+        );
+
+
+    };
+    reader.readAsBinaryString(file);
+}
+
+function error(msg, error) {
+    //TODO: @Quantompixel: Create UI for this :)
+    console.error(error);
+    window.alert(msg + "\n" + JSON.stringify(error));
+}
+
+function read(e) {
+    const file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    try {
+        if (file.name.endsWith(".json")) {
+            loadFromJSON(file);
+        } else {
+            loadFromImage(file);
+        }
+    } catch (e) {
+        error("Could not load the File!", e);
+    }
 }
 
 function saveData() {
